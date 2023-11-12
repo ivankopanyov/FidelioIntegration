c503341ab32726e6d39ab98191f80dfb029affd5
ivankopanyov <i.kopanyov@gmail.com>
2023-11-12 10:45:38 +0300

    Create FidelioIntegration.Opera.Domain.Entities.Tables

---
+++ src/Opera/Domain/Entities/FidelioIntegration.Opera.Domain.Entities.Tables/Entities/SysIotOver142045.cs
@@ -0,0 +1,15 @@
+namespace FidelioIntegration.Opera.Domain.Entities.Tables;
+
+public partial class SysIotOver142045
+{
+
+	public static void OnModelCreating(ModelBuilder modelBuilder, ISet<Type> types)
+	{
+		modelBuilder.Entity<SysIotOver142045>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.ToTable("SYS_IOT_OVER_142045");
+        });
+	}
+}
