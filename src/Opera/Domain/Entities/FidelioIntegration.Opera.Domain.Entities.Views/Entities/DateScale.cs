510bca60bb4ac95b6cfa59168c5d84713f386103
ivankopanyov <i.kopanyov@gmail.com>
2023-11-12 10:49:02 +0300

    Create FidelioIntegration.Opera.Domain.Entities.Views

---
+++ src/Opera/Domain/Entities/FidelioIntegration.Opera.Domain.Entities.Views/Entities/DateScale.cs
@@ -0,0 +1,18 @@
+namespace FidelioIntegration.Opera.Domain.Entities.Views;
+
+public partial class DateScale
+{
+    public DateTime? D { get; set; }
+
+	public static void OnModelCreating(ModelBuilder modelBuilder, ISet<Type> types)
+	{
+		modelBuilder.Entity<DateScale>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.ToView("DATE_SCALE");
+
+            entity.Property(e => e.D).HasColumnType("DATE");
+        });
+	}
+}
---
+++ src/Opera/Domain/Entities/FidelioIntegration.Opera.Domain.Entities.Views/Entities/RequestSendMethods.cs
@@ -0,0 +1,26 @@
+namespace FidelioIntegration.Opera.Domain.Entities.Views;
+
+public partial class RequestSendMethods
+{
+    public string? SendMethod { get; set; }
+    public string? Description { get; set; }
+
+	public static void OnModelCreating(ModelBuilder modelBuilder, ISet<Type> types)
+	{
+		modelBuilder.Entity<RequestSendMethods>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.ToView("REQUEST_SEND_METHODS");
+
+            entity.Property(e => e.Description)
+                .HasColumnName("DESCRIPTION")
+                .IsUnicode(false);
+
+            entity.Property(e => e.SendMethod)
+                .HasColumnName("SEND_METHOD")
+                .HasMaxLength(5)
+                .IsUnicode(false);
+        });
+	}
+}
