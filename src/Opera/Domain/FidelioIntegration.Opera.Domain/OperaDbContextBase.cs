b604d20220a6f56a51bcd4bd49e24900cb7c1e9b
ivankopanyov <i.kopanyov@gmail.com>
2023-11-12 11:23:35 +0300

    Bugfix FidelioIntegration.Opera.Domain

--- src/Opera/Domain/FidelioIntegration.Opera.Domain/OperaDbContextBase.cs
+++ src/Opera/Domain/FidelioIntegration.Opera.Domain/OperaDbContextBase.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace FidelioIntegration.Opera.Domain;
 
-namespace FidelioIntegration.Opera.Domain
+public abstract class OperaDbContext : DbContext
 {
-    internal class OperaDbContextBase
+    private static ISet<Type>? _types;
+
+    private static IEnumerable<MethodInfo>? _methods;
+
+    protected sealed override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        _types ??= GetType().GetProperties()
+            .Where(property => property.PropertyType.Name == "DbSet`1" &&
+                property.PropertyType.GenericTypeArguments.Any())
+            .Select(property => property.PropertyType.GenericTypeArguments.First())
+            .ToHashSet();
+
+        _methods ??= _types
+            .SelectMany(type => type.GetMethods()
+                .Where(method => method.Name == nameof(OnModelCreating) &&
+                    method.IsStatic &&
+                    method.GetParameters().Length == 2 &&
+                    method.GetParameters().Any(parameter => parameter.ParameterType == typeof(ModelBuilder)) &&
+                    method.GetParameters().Any(parameter => parameter.ParameterType == typeof(ISet<Type>))));
+
+        foreach (var method in _methods)
+            method.Invoke(null, new object[] { modelBuilder, _types });
     }
 }
+
